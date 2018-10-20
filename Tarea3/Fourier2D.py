276b59b269f1392ec616460aaf28e39ebd5d0871
Cindy Hernández <ca.hernandez14@uniandes.edu.co>
2018-10-20 13:23:41 -0500

    Punto 3 Completo

---
+++ Tarea3/Fourier2D.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from scipy import fftpack
+from scipy.fftpack import fft, fftfreq
+
+arbol = plt.imread('Arboles.png')
+arbol_fft = fftpack.fft2(arbol)
+plt.figure()
+plt.imshow(np.abs(arbol_fft), norm=LogNorm(vmin=1))
+plt.colorbar()
+plt.savefig('HernandezCindy_FT2D.pdf')
+
+r, c = arbol_fft.shape
+a_filter = 23
+arbol_fft[:,a_filter:r-a_filter] = 0
+arbol_fft[:,a_filter:r-a_filter] = 0
+plt.figure()
+plt.imshow(np.abs(arbol_fft), norm=LogNorm(vmin=1))
+plt.colorbar()
+plt.savefig('HernandezCindy_FT2D_filtrada.pdf')
+
+arbol_filtrada = fftpack.ifft2(arbol_fft)
+
+plt.figure()
+plt.imshow(abs(arbol_filtrada), plt.cm.gray)
+plt.savefig('HernandezCindy_Imagen_filtrada.pdf')
