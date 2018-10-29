c8066082b90e75fe11e8750e8409e8ab487a34c3
Cindy Hernández <ca.hernandez14@uniandes.edu.co>
2018-10-29 10:30:29 -0500

    PCA corregido

--- Tarea3/PCR.py
+++ Tarea3/PCR.py
@@ -6,18 +6,13 @@ link = 'http://ftp.cs.wisc.edu/math-prog/cpo-dataset/machine-learn/cancer/WDBC/W
 
 #Leer datos en formato float64 y str
 data_numbers = np.genfromtxt(link,delimiter =',')
-data_text    = np.genfromtxt(link,delimiter =',', dtype = 'str')
+tag          = np.genfromtxt(link,delimiter =',', dtype = 'str')
 
 #Combinar datos float64 y str
-data = np.empty(data_numbers.shape)
-data = data_numbers.copy()
+data = np.delete(data_numbers, 1, 1)
 
-#Binarizar los datos
-for i in range(data.shape[0]):
-    if data_text[i,1] == 'B':
-        data[i,1] = 1
-    else:
-        data[i,1] = 0
+data_text = tag[:,1]
+
         
 data2 = np.zeros(data.shape)
 
@@ -42,8 +37,8 @@ print('Los componentes mas importantes se encuentran en los dos ultimos datos')
 nuevos_vectores = np.dot(data2,vectores_propios[:,-2:])
     
 plt.figure()
-plt.scatter(nuevos_vectores[:,0][np.where(data[:,1] == 0)], nuevos_vectores[:,1][np.where(data[:,1] == 0)], label = 'Benigno', alpha=0.4, c='g')
-plt.scatter(nuevos_vectores[:,0][np.where(data[:,1] == 1)], nuevos_vectores[:,1][np.where(data[:,1] == 1)], label = 'Maligno', alpha=0.2, c='r')
+plt.scatter(nuevos_vectores[:,0][np.where(data_text == 'B')], nuevos_vectores[:,1][np.where(data_text == 'B')], label = 'Benigno', alpha=0.4, c='g')
+plt.scatter(nuevos_vectores[:,0][np.where(data_text == 'M')], nuevos_vectores[:,1][np.where(data_text == 'M')], label = 'Maligno', alpha=0.2, c='r')
 plt.legend()
 plt.savefig('HernandezCindy_PCA.pdf')
 
